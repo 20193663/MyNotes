15be630bae93e00580d9a4bba6d820822634d365
20193663 Ibrahim Ayman Ibrahim <73901605+20193663@users.noreply.github.com>
2023-05-15 01:02:54 +0300

    add first draft of login page

--- app/src/main/java/com/fue/mynotes/Signup.java
+++ app/src/main/java/com/fue/mynotes/Signup.java
@@ -5,30 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView;
+public class Signup{}
+/*public class Signup  extends AppCompatActivity {
     Button login,signup;
-    EditText email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login  =  findViewById(R.id.LoginButton);
         signup = findViewById(R.id.SignupButton);
-        email = findViewById(R.id.EmailAddress);
-        password = findViewById(R.id.password);
+        //TODO delete this
+        TextView t = findViewById(R.id.loginpagetitel);
+        t.setText("Signup");
         login.setOnClickListener(v -> {
             // Your onClick implementation here.
-            Intent myIntent = new Intent(MainActivity.this, Signup.class);
+            Intent myIntent = new Intent(Signup.this, MainActivity.class);
             //myIntent.putExtra("key", value); //Optional parameters
-            MainActivity.this.startActivity(myIntent);
+            Signup.this.startActivity(myIntent);
         });
         signup.setOnClickListener(v -> {
             // Your onClick implementation here.
-            Intent myIntent = new Intent(MainActivity.this, Signup.class);
+            Intent myIntent = new Intent(Signup.this, MainActivity.class);
             //myIntent.putExtra("key", value); //Optional parameters
-            MainActivity.this.startActivity(myIntent);
+            Signup.this.startActivity(myIntent);
         });
     }
 }
+*/
