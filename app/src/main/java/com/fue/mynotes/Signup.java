964a4ede1a04ca75a0aefbd636fc70d06bb90f1d
20193663 Ibrahim Ayman Ibrahim <73901605+20193663@users.noreply.github.com>
2023-05-15 09:16:34 +0300

    fix error in gradle

--- app/src/main/java/com/fue/mynotes/Signup.java
+++ app/src/main/java/com/fue/mynotes/Signup.java
@@ -5,19 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class Signup  extends AppCompatActivity {
     Button login,signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
         login  =  findViewById(R.id.LoginButton);
         signup = findViewById(R.id.SignupButton);
-        //TODO delete this
-        TextView t = findViewById(R.id.loginpagetitel);
-        t.setText("Signup");
         login.setOnClickListener(v -> {
             // Your onClick implementation here.
             Intent myIntent = new Intent(Signup.this, MainActivity.class);
